0f6a93bd1c1fceb180bd43248b6817b87aa032e7
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-07-28 12:39:24 +0530

    completed patterns chapter2

--- Patterns2/patterns.java
+++ Patterns2/patterns.java
@@ -71,16 +71,16 @@ public class patterns {
             0 1 0 1
             1 0 1 0 1 
         */
-        for(int i=1; i<=5; i++) {
-            for(int j=1; j<=i; j++) {
-                if((i+j)%2==0) {
-                    System.out.print(1+" ");
-                } else {
-                    System.out.print(0+ " ");
-                }
-            }
-            System.out.println();
-        }
+        // for(int i=1; i<=5; i++) {
+        //     for(int j=1; j<=i; j++) {
+        //         if((i+j)%2==0) {
+        //             System.out.print(1+" ");
+        //         } else {
+        //             System.out.print(0+ " ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
         /*
             *             *
             * *         * *
@@ -91,6 +91,98 @@ public class patterns {
             * *         * *
             *             *
         */
-        
+        // int n=10;
+        // for(int i=1; i<=n; i++) {
+        //     for(int j=1; j<=i; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     for(int j=1; j<=2*(n-i); j++) {
+        //         System.out.print("  ");
+        //     }
+        //     for(int j=1; j<=i; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+        // for(int i=n; i>=1; i--) {
+        //     for(int j=1; j<=i; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     for(int j=1; j<=2*(n-i); j++) {
+        //         System.out.print("  ");
+        //     }
+        //     for(int j=1; j<=i; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        /*
+                *****
+               *****
+              *****
+             *****
+            *****
+        // */
+        // for(int i=1; i<=5; i++) {
+        //     for(int j=1; j<=5-i; j++) {
+        //         System.out.print("  ");
+        //     }
+        //     for(int j=1; j<=5; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        /*
+                *****
+               *   *
+              *   *
+             *   *
+            *****
+        */
+        // for(int i=1; i<=5; i++) {
+        //     for(int j=1; j<=5-i; j++) {
+        //         System.out.print("  ");
+        //     }
+        //     for(int j=1; j<=5; j++) {
+        //         if(i==1 || i==5 || j==1 || j==5) {
+        //             System.out.print("* ");
+        //         } else {
+        //             System.out.print("  ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        /*
+               *
+              ***
+             *****
+            *******
+            *******
+             *****
+              ***
+               *
+        */
+        int n=8;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=(2*i)-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n; i>=1; i--) {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=(2*i)-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
