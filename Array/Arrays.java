f14c55b1bf5e1a1d584bfb7357a240f2932077fc
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-07-29 05:35:36 +0530

    completed till Reverse an array

--- Array/Arrays.java
+++ Array/Arrays.java
@@ -28,10 +28,49 @@ public class Arrays {
 
         return lar;
     }
-    /*
-        Largest Number:
-        find the largest number in a given array
-    */
+    public static int binarySearch(int arr[], int key) {
+        /*  
+            Binary Search
+            prerequisite - sorted arrays
+        */
+        int l=arr[0], r=arr.length;
+        while (l<=r) {
+            int mid = (l+r)/2;
+            if(arr[mid]>key) {
+                r=mid-1;
+            } else if(arr[mid]<key) {
+                l=mid+1;
+            } else {
+                return mid;
+            }
+        }  
+        return -1;
+    }
+    public static void printArray(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+ " ");
+        }
+        return;
+    }
+    public static void reverseArr(int arr[]) {
+        //Reverse an Array
+        int n = arr.length;
+        for(int i=0; i<n/2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n-i-1];
+            arr[n-i-1] = temp;
+        }
+        /*
+            int first = 0, last = numbers.length—1;
+            while(first < last) {
+                int temp = numbers[last];
+                numbers[last] = numbers [first[];
+                numbers [first] = temp;
+                first++;
+                last--; 
+            }
+        */
+    }
     public static void main(String[] args) {
         /*
             Creating an Array
@@ -48,7 +87,17 @@ public class Arrays {
         // linearSearch(arr, key);
 
         //lagest number
+        // int arr[] = {1, 2, 6, 3, 5};
+        // System.out.println("largest number is "+largestNum(arr));
+
+        //binary search
+        // int arr[] = {2, 4, 6, 8, 10, 12, 14};
+        // int key = 0;
+        // System.out.println("found at index "+binarySearch(arr, key));
+
+        //Reverse an Array
         int arr[] = {1, 2, 6, 3, 5};
-        System.out.println("largest number is "+largestNum(arr));
+        reverseArr(arr);
+        printArray(arr);
     }
 }
