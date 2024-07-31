8d9bc51bef948eb3c531739848c5a0b909306971
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-08-01 04:59:35 +0530

    completed LargestlnAnArray from gfg

---
+++ Array/LargestInAnArray.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class LargestInAnArray {
+    public static int largest(int n, int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<n; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+        System.out.println(largest(5, arr));
+    }    
+}
