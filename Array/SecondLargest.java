ba41bc30659934d247a6c2d7b4411d6c96892434
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-08-01 05:32:21 +0530

    completed SecondLargest from gfg

---
+++ Array/SecondLargest.java
@@ -0,0 +1,30 @@
+package Array;
+/**
+ * SecondLargest
+ */
+public class SecondLargest {
+    public void print2largest(int arr[]) {
+        int small, ssmall, large, slarge;
+        small = Integer.MAX_VALUE;
+        ssmall = Integer.MAX_VALUE;
+        large = Integer.MIN_VALUE;
+        slarge = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++) {
+            small = Math.min(small, arr[i]);
+            large = Math.max(large, arr[i]);
+        }
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i]<ssmall && arr[i]!=small) {
+                ssmall = arr[i];
+            }
+            if(arr[i]>slarge && arr[i]!=large) {
+                slarge = arr[i];
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        
+    }    
+    
+}
