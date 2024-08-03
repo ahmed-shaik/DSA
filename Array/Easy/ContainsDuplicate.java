4460f8b09265face11f6ade5cf18d0b42abac3aa
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-08-03 18:13:15 +0530

    completed containsDuplicate

---
+++ Array/Easy/ContainsDuplicate.java
@@ -0,0 +1,18 @@
+package Array.Easy;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
