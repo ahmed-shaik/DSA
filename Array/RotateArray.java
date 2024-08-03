5c2a2816e9c61d2980e00f83e41ddb0269d6324a
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-08-03 15:08:28 +0530

    completed RotateArray from lc

---
+++ Array/RotateArray.java
@@ -0,0 +1,19 @@
+package Array;
+
+/**
+ * RotateArray
+ */
+public class RotateArray {
+        public void rotate(int[] nums, int k) {
+            int a[]= new int[nums.length];
+            for(int i=0; i<nums.length;i++) {
+                a[(i+k)%nums.length] = nums[i];
+            }
+            for(int i=0; i<nums.length; i++) {
+                nums[i] = a[i];
+            }
+        }
+    public static void main(String[] args) {
+        
+    }
+}
