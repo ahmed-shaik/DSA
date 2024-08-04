2e080f6ff56a6ff0d94e9b4b593be10006689582
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-08-04 15:17:36 +0530

    completed collecting chocloates from lc

---
+++ Array/Easy/Collectingchocolates.java
@@ -0,0 +1,23 @@
+package Array.Easy;
+
+public class Collectingchocolates {
+    class Solution {
+        public long minCost(int[] n, int x) {
+            long ans = 0L, tmp;
+            int[] m = new int[n.length];
+            for(int i=0; i<n.length; i++){m[i]=n[i]; ans+=n[i];}
+            for(int j=1; j<n.length; j++){
+                tmp= (long)j * x;;
+                for(int i=0; i<n.length; i++){
+                    m[i] = Math.min(m[i], n[(i+j)%n.length]);
+                    tmp+=m[i];
+                }
+                if(ans>tmp)ans=tmp;
+            }
+            return ans;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
