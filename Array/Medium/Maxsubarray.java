e5b586a800fdb85d89b70d50132a4a1f5fb77612
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-08-07 05:31:15 +0530

    Completed maximum subarray from lc

---
+++ Array/Medium/Maxsubarray.java
@@ -0,0 +1,18 @@
+package Array.Medium;
+
+public class Maxsubarray {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE, cs=0;
+        for(int i=0; i<nums.length; i++) {
+            cs+=nums[i];
+            if(cs<0) {
+                cs=0;
+            }
+            max = Math.max(max, cs);
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
