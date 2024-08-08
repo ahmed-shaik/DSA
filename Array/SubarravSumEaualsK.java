771b1ef684b1466aac33783c8685cf504e674ef8
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-08-09 05:27:57 +0530

    completed sub array sum equals k from lc

---
+++ Array/SubarravSumEaualsK.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class SubarravSumEaualsK {
+    public int subarraySum(int[] nums, int k) {
+        int c=0;
+        int prefixsum[] = new int[nums.length];
+        prefixsum[0] = nums[0];
+        for(int i=1; i<nums.length; i++) {
+            prefixsum[i] = nums[i] + prefixsum[i-1];
+        }
+        for(int i=0; i<nums.length; i++) {
+            for(int j=1; j<nums.length; j++) {
+                int cs = i==0 ? prefixsum[j] : prefixsum[j] - prefixsum[i-1];
+                if(cs==k) 
+                    c++;
+            }
+        }
+        return c;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
