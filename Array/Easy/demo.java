434ddd063c37fa7927e58c1da4f686fa2ea82f3f
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-08-16 11:02:00 +0530

    commit

---
+++ Array/Easy/demo.java
@@ -0,0 +1,16 @@
+package Array.Easy;
+
+import java.util.Arrays;
+
+public class demo {
+    public static void main(String[] args) {
+        int arr [][] = new int[2][2];
+        Arrays.sort(arr);
+        for(int i=0; i<arr.length; i++) {
+            for(int j=0; j<arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
