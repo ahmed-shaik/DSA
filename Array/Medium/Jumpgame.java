af4af8d86a4afc8304fddbabfbfd1372837341d6
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-08-22 05:33:29 +0530

    completed Jump Game from lc

---
+++ Array/Medium/Jumpgame.java
@@ -0,0 +1,18 @@
+package Array.Medium;
+
+/**
+ * Jumpgame
+ */
+public class Jumpgame {
+    public boolean canJump(int[] nums) {
+        int reachable = 0;
+        for(int i = 0; i < nums.length; i ++) {
+            if(i > reachable) return false;
+            reachable = Math.max(reachable, i + nums[i]);
+        } 
+        return true;
+     }
+    public static void main(String[] args) {
+        
+    }
+}
