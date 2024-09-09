0a15fe1099bcffb6dafe80df1ec2ac3452b8d3a9
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-09-09 07:05:33 +0530

    completed basic sorting chapter

--- Sorting/Sortingg.java
+++ Sorting/Sortingg.java
@@ -1,4 +1,5 @@
 package Sorting;
+// import java.util.Arrays;
 
 public class Sortingg {
     public static void bubbleSort(int arr[]) {
@@ -18,14 +19,62 @@ public class Sortingg {
             }
         }
     }
+    public static void selectionSort(int arr[]) {
+        for(int i=0; i<arr.length-1; i++) {
+            int sm = i;
+            for(int j=i+1; j<arr.length; j++) {
+                if(arr[j]<arr[sm]) {
+                    sm = j;
+                }
+            }
+            int temp = arr[sm];
+            arr[sm] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        for(int i=1; i<arr.length; i++) {
+            int curr=arr[i];
+            int prev=i-1;
+            while(prev>=0 && arr[prev]>curr) {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+        }
+    }
+
+    public static void countingSort(int arr[]) {
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        int count [] = new int[max+1];
+        for(int i=0; i<arr.length; i++) {
+            count[arr[i]]++;
+        }
+        int j=0;
+        for(int i=0; i<count.length; i++) {
+            while (count[i]>0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
     public static void printArray(int [] arr) {
         for(int i=0; i<arr.length; i++) {
             System.out.print(arr[i]+" ");
         }
     }
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5};
-        bubbleSort(arr);
+        int arr[] = {1,4,1,3,2,4,3,7};
+        // bubbleSort(arr);
+        // selectionSort(arr);
+        // insertionSort(arr);
+        // Arrays.sort(arr);
+        countingSort(arr);
         printArray(arr);
     }
 }
