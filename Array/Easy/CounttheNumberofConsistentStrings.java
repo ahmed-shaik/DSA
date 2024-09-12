746d488052ea86b92e79b8bcff8214805320cbaf
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-09-12 05:47:22 +0530

    completed MinimumBitF1ipstoConvertNumber and conthte numberofconsistentstrings from lc

---
+++ Array/Easy/CounttheNumberofConsistentStrings.java
@@ -0,0 +1,23 @@
+package Array.Easy;
+
+public class CounttheNumberofConsistentStrings {
+    public int countConsistentStrings(String allowed, String[] words) {
+        int count = words.length;
+        int[] store = new int[26];
+    
+        for (char c : allowed.toCharArray()){
+            store[c-'a']++;
+        }
+    
+        for (String word : words) {
+            
+            for (char c : word.toCharArray()) {
+                if (store[c-'a'] <= 0){
+                    count--;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
---
+++ Bit_Manipulation/MinimumBitFlipstoConvertNumber.java
@@ -0,0 +1,21 @@
+package Bit_Manipulation;
+
+public class MinimumBitFlipstoConvertNumber {
+    public int minBitFlips(int start, int goal) {
+        int ans = 0; 
+        // XOR will give 1 where the bits are different
+        int xor = start ^ goal;
+
+        while(xor!=0){
+            // If the last bit is 1, increment count 
+            ans += xor & 1;
+
+            // Right-shift to check the next bit
+            xor >>=1;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
