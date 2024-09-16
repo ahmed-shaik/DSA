eec7ef3b810085c74127df078377a876cca55bc1
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-09-16 18:49:34 +0530

    c

---
+++ New.java
@@ -0,0 +1,9 @@
+public class New {
+
+    public static void main(String[] args) {
+        String str[] = {"eat","tea","tan","ate","nat","bat"};
+        str.toString();
+        System.out.println(str[0].toCharArray());
+    }
+    
+}
