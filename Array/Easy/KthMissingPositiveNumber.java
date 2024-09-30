10881da395c6b82e1c8811e1eacd0bd4d2a3ec5a
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-09-30 05:49:18 +0530

    commit

--- Array/Easy/KthMissingPositiveNumber.java
+++ Array/Easy/KthMissingPositiveNumber.java
@@ -9,6 +9,7 @@ public class KthMissingPositiveNumber {
         public int findKthPositive(int[] arr, int k) {
             int left = 0, right = arr.length - 1;
             while (left <= right) {
+                
                 int mid = left + (right - left) / 2;
                 if (arr[mid] - mid - 1 < k) {
                     left = mid + 1;
