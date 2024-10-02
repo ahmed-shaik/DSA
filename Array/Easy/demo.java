35c1a51c211a70d6f63771320f244e728ed75eb0
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-10-02 12:27:23 +0530

    c

--- Array/Easy/demo.java
+++ Array/Easy/demo.java
@@ -6,6 +6,7 @@ public class demo {
     public static void main(String[] args) {
         int arr [][] = new int[2][2];
         Arrays.sort(arr);
+        
         for(int i=0; i<arr.length; i++) {
             for(int j=0; j<arr[0].length; j++) {
                 System.out.print(arr[i][j] + " ");
