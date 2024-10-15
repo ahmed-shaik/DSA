7ae7a3c8f6ef1e526f7d43a0054839533a8dbf48
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-10-16 05:20:49 +0530

    Resumed Recursion chapter

--- Recursion_Basics/Recursion_basic.java
+++ Recursion_Basics/Recursion_basic.java
@@ -20,9 +20,36 @@ public class Recursion_basic {
         }
         return n + sumOfFirstN(n-1);        
     }
+    public static int fib(int n) { 
+        if(n==0 || n==1) {
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+    public static boolean isSorted(int arr[], int i) {
+        if(i==arr.length-1) {
+            return true;
+        }
+        if(arr[i]<arr[i+1]) {
+            return false;
+        }
+        return isSorted(arr, i+1);
+    }
+    public static int firstOcc(int arr[], int i, int tar) {
+        if(i==arr.length) {
+            return -1;
+        }
+        if(arr[i]== tar) {
+            return i;
+        }
+        return firstOcc(arr, i+1, tar);
+    }
     public static void main(String[] args) {
         // PrintUpto10(10);
         // System.out.println(fact(3));
-        System.out.println(sumOfFirstN(5));
+        // System.out.println(sumOfFirstN(5));
+        // System.out.println(fib(7));
+        // System.out.println(isSorted(new int[]{1,2,9,4,9,5}, 0));
+        System.out.println(firstOcc(new int[]{1,2,9,4,9,5}, 0, 8));
     }
 }
