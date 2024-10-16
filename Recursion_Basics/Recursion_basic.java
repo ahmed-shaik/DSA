b4e6de915f65dd43a051a9576a46c775c4b633c9
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-10-17 05:26:26 +0530

    Resumed Recursion Chapter

--- Recursion_Basics/Recursion_basic.java
+++ Recursion_Basics/Recursion_basic.java
@@ -44,12 +44,60 @@ public class Recursion_basic {
         }
         return firstOcc(arr, i+1, tar);
     }
+    public static int lastOcc1(int arr[], int i, int tar, int val) {
+        if(i==arr.length) {
+            return val;
+        }
+        if(arr[i]== tar) {
+            val = i;
+        }
+        return lastOcc1(arr, i+1, tar, val);
+    }
+    public static int lastOcc2(int arr[], int i, int tar) {
+        if(i==arr.length) {
+            return -1;
+        }
+        int isFound = lastOcc2(arr, i+1, tar);
+        if(isFound != -1) {
+            return isFound;
+        }
+        if(arr[i]== tar) {
+            return i;
+        }
+        return isFound;
+    }
+    public static int printxPn(int x, int n) {
+        if(n==1) {
+            return x;
+        }
+        // return x*printxPn(x, n-1);
+
+        int halfpower = printxPn(x, n/2);
+        int halfPowerSq = halfpower*halfpower;
+        if(n%2==0) {
+            return halfPowerSq; 
+        } else {
+            return halfPowerSq*x;
+        }
+        // return n%2==0? halfpower*halfpower : halfpower*halfpower * x;
+    }
+    public static int tilingProblem(int n) {
+        if(n==0 || n==1) {
+            return 1;
+        }
+        return tilingProblem(n-1) + tilingProblem(n-2);
+    }
     public static void main(String[] args) {
         // PrintUpto10(10);
         // System.out.println(fact(3));
         // System.out.println(sumOfFirstN(5));
         // System.out.println(fib(7));
         // System.out.println(isSorted(new int[]{1,2,9,4,9,5}, 0));
-        System.out.println(firstOcc(new int[]{1,2,9,4,9,5}, 0, 8));
+        // System.out.println(firstOcc(new int[]{1,2,9,4,9,5}, 0, 8));
+        // System.out.println(lastOcc1(new int[]{1,2,9,4,9,5}, 0, 8, -1));
+        // System.out.println(lastOcc2(new int[]{1,2,9,4,9,5}, 0, 9));
+        // System.out.println(printxPn(2, 10));
+        System.out.println(tilingProblem(3));
+
     }
 }
