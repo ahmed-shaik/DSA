5f95060c3a7202467304869adbdd2a0b9c6a23a1
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-10-19 05:42:28 +0530

    Completed Power of 3 From lc

---
+++ Recursion_Basics/PowerOfThree.java
@@ -0,0 +1,15 @@
+package Recursion_Basics;
+
+public class PowerOfThree {
+    class Solution {
+        public boolean isPowerOfThree(int n) {
+            if(n<1) {
+                return false;
+            }
+            if(n==1) {
+                return true;
+            }
+            return (n%3==0) && isPowerOfThree(n/3);
+        }
+    }
+}
