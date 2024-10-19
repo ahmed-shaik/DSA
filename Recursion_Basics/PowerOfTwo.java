f4195be8e9ea4df665182ad6def028e3789ac6d4
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-10-19 05:41:38 +0530

    Completed Power of 2 From lc

---
+++ Recursion_Basics/PowerOfTwo.java
@@ -0,0 +1,15 @@
+package Recursion_Basics;
+
+public class PowerOfTwo {
+    class Solution {
+        public boolean isPowerOfTw0(int n) {
+            if(n<1) {
+                return false;
+            }
+            if(n==1) {
+                return true;
+            }
+            return (n%2==0) && isPowerOfTw0(n/2);
+        }
+    }
+}
