35141076b8fecd05dca4451315e1c6113e85176d
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-12-24 06:03:45 +0530

    Completed Quick sort

--- DivideConquer/DivideConquer_Chapter.java
+++ DivideConquer/DivideConquer_Chapter.java
@@ -30,9 +30,34 @@ public class DivideConquer_Chapter {
             arr[i] = temp[k];
         }
     }
+    //Quick sort
+    public static void quickSort(int [] arr, int si, int ei) {
+        if(si<=ei) {
+            return;
+        }
+        int pi=partition(arr, si, ei);
+        quickSort(arr, si, pi-1);
+        quickSort(arr, pi+1, ei);
+    }
+    public static int partition(int [] arr, int si, int ei) {
+        int p=arr[ei],i=si-1;
+        for(int j=si; j<ei; j++) {
+            if(arr[j]<=p) {
+                i++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        int temp = arr[ei];
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
     public static void main(String[] args) {
         int [] arr = {6,3,9,5,2,8};
-        mergeSort(arr, 0, arr.length-1);
+        quickSort(arr, 0, arr.length-1);
         for(int i=0; i<arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
