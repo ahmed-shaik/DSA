9d660b9e6030cece18dbbde0d628b9ce7f18cf36
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-12-27 05:16:21 +0530

    completed merge sort for strings

--- DivideConquer/DivideConquer_chapterr.java
+++ DivideConquer/DivideConquer_chapterr.java
@@ -63,7 +63,7 @@ public class DivideConquer_chapterr {
         if(arr[mid] == tar) {
             return mid;
         }
-        if(arr[si]<=mid) {
+        if(arr[si]>=mid) {
             if(arr[si] <= tar && tar<=arr[mid]) {
                 return searchInRotatedSortedArray(arr, tar, si, mid-1);
             } else {
@@ -75,6 +75,37 @@ public class DivideConquer_chapterr {
             } else {
                 return searchInRotatedSortedArray(arr, tar, si, mid-1);
             }
+        }
+    }
+    public static void mergeSort(String arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+        int mid = si + (ei - si) / 2;
+        
+        mergeSort(arr, si, mid);
+        mergeSort(arr, mid + 1, ei);
+        merge(arr, si, mid, ei);
+    }
+
+    public static void merge(String arr[], int si, int mid, int ei) {
+        String temp[] = new String[ei - si + 1];
+        int i = si, j = mid + 1, k = 0;
+        while (i <= mid && j <= ei) {
+            if (arr[i].compareTo(arr[j]) < 0) { 
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+        }
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
         }
     }
     public static void main(String[] args) {
