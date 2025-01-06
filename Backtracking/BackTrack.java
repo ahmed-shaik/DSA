6e8f1706da0a9d443369e06c15b863707e3a6b09
ahmed-shaik <ahmedshaikvk18@gmail.com>
2025-01-06 05:30:12 +0530

    Completed Nqueens

--- Backtracking/BackTrack.java
+++ Backtracking/BackTrack.java
@@ -32,11 +32,59 @@ public class BackTrack {
             System.out.print(arr[i]+" ");
         }
     } 
+    public static void nQueens(char board[][], int row) {
+        if(row==board.length) {
+            printBoard(board);
+            return;
+        }
+        for(int j=0; j<board.length; j++) {
+            if(isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueens(board, row+1);
+                board[row][j] = 'x';
+            }
+        }   
+    }
+    public static boolean isSafe(char board[][], int row, int col) {
+        for(int i=row-1; i>=0; i--) {
+            if(board[i][col]=='Q') {
+                return false;
+            }
+        }
+        for(int i=row-1,j=col-1; i>=0&& j>=0; i--,j--) {
+            if(board[i][j]=='Q') {
+                return false;
+            }
+        }
+        for(int i=row-1, j=col+1; i>=0 && j<board.length; i--,j++) {
+            if(board[i][j]=='Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void printBoard(char board[][]) {
+        System.out.println("___________________________________________________");
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board.length; j++) {
+                System.out.print(board[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         // int arr[] = new int[5];
         // backTrackArray(0, arr);
         // printArray(arr);
         // findSubSets("abc", 0, "");
-        findPermutations("abc", "");
+        // findPermutations("abc", "");
+        int n=5;
+        char board [][] = new char[n][n];
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                board[i][j] = 'x';
+            }
+        }
+        nQueens(board, 0);
     }
 }
