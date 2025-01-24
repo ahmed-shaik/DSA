c0e6e591f4c9b0578ef2deccf96487a320a1c3a3
ahmed-shaik <ahmedshaikvk18@gmail.com>
2025-01-24 05:33:55 +0530

    completed from Find All Lonely Numbers in the Array leetcode

---
+++ ArrayLists/FindAllLonelyNumbersintheArray.java
@@ -0,0 +1,22 @@
+package ArrayLists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindAllLonelyNumbersintheArray {
+    class Solution {
+    public List<Integer> findLonely(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0 ; i < nums.length ; i++){
+           if((i==0 || nums[i] - nums[i-1] > 1) &&(i==nums.length-1 || nums[i+1] - nums[i] > 1)){
+            list.add(nums[i]);
+           }
+          
+        }
+        return list;
+
+    }
+}
+}
