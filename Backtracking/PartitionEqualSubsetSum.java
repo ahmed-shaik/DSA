f2445e4f40d1cde2caa272406024e05586f6a4cf
ahmed-shaik <ahmedshaikvk18@gmail.com>
2025-02-06 06:17:01 +0530

    completd from Partition Equal Subset Sum lc but will have to do it using dynamic programmin

--- Backtracking/PartitionEqualSubsetSum.java
+++ Backtracking/PartitionEqualSubsetSum.java
@@ -1,5 +1,32 @@
 package Backtracking;
 
 public class PartitionEqualSubsetSum {
-    
+    class Solution {
+        public boolean canPartition(int[] nums) {
+            int sum=0;
+            for(int i:nums) {
+                sum+=i;
+            }
+            if(sum%2!=0) {
+                return false;
+            }
+            boolean []used = new boolean[nums.length];
+            return backtrack(0, 0, used, nums, sum/2);
+        }
+        public static boolean backtrack(int start, int currsum, boolean [] used, int[]nums, int tar) {
+            if(currsum==tar) {
+                return true;
+            }
+            for(int i=start; i<nums.length; i++) {
+                if(!used[i] && currsum+nums[i]<=tar) {
+                    used[i]=true;
+                    if(backtrack(i+1, currsum+nums[i], used, nums, tar)) {
+                        return true;
+                    }
+                    used[i]=false;
+                }
+            }
+            return false;
+        }
+    }
 }
