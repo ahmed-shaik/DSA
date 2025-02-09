5c375c289f90b7047b4d231106aaf383082ba92a
ahmed-shaik <ahmedshaikvk18@gmail.com>
2025-02-09 12:17:28 +0530

    started linked list chapter

---
+++ Linkedlist/LinkedList.java
@@ -0,0 +1,52 @@
+package Linkedlist;
+
+public class LinkedList {
+    public static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+    public void printList() {
+        Node temp = head;
+        while(temp.next!=null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.print("null");
+    }
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        ll.addFirst(6);
+        ll.addLast(0);
+        ll.addLast(-1);
+        ll.printList();
+    }
+}
