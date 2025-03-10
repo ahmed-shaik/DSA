8a55a9d61862ddc8aa61cbbf91f66938c82fd85b
Ahmed <ahmedshaikvk18@gmail.com>
2025-03-10 06:53:41 +0530

    completed delete nth node from last fro lc

---
+++ Linkedlist/RemoveNthNodeFromEndofList.java
@@ -0,0 +1,34 @@
+package Linkedlist;
+
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+public class RemoveNthNodeFromEndofList {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        // public ListNode removeNthFromEnd(ListNode head, int n) {
+        // ListNode temp = head;
+        // int len = 0;
+        // while (temp != null) {
+        // len++;
+        // temp = temp.next;
+        // }
+        // if (n == len) {
+        // head = head.next;
+        // return head;
+        // }
+        // temp = head;
+        // for (int i = 0; i < len - n - 1; i++) {
+        // temp = temp.next;
+        // }
+        // temp.next = temp.next.next;
+        // return head;
+    }
+}
