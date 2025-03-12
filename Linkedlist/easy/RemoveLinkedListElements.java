fefe65f9199af88851604986c02642a2d641bdaf
Ahmed <ahmedshaikvk18@gmail.com>
2025-03-12 12:22:07 +0530

    Completed Remove Linked List Elements from leetcode

---
+++ Linkedlist/easy/RemoveLinkedListElements.java
@@ -0,0 +1,26 @@
+// package Linkedlist.easy;
+
+// public class RemoveLinkedListElements {
+// class Solution {
+// public ListNode removeElements(ListNode head, int val) {
+// if (head == null) {
+// return head;
+// }
+
+// ListNode dummy = new ListNode(0);
+// dummy.next = head;
+// ListNode curr = dummy;
+
+// while (curr != null && curr.next != null) {
+
+// if (curr.next.val == val) {
+// curr.next = curr.next.next;
+// } else {
+// curr = curr.next;
+// }
+// }
+// return dummy.next;
+// }
+// }
+
+// }
