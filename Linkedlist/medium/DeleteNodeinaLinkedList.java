f322bbb03421fe5fc42d1b29b14bf7130daf498b
Ahmed <ahmedshaikvk18@gmail.com>
2025-03-28 12:02:43 +0530

    completed Delete Node in a Linked List from leetcode

---
+++ Linkedlist/medium/DeleteNodeinaLinkedList.java
@@ -0,0 +1,18 @@
+// package Linkedlist.medium;
+
+// public class DeleteNodeinaLinkedList {
+// /**
+// * Definition for singly-linked list.
+// * public class ListNode {
+// * int val;
+// * ListNode next;
+// * ListNode(int x) { val = x; }
+// * }
+// */
+// class Solution {
+// public void deleteNode(ListNode node) {
+// node.val = node.next.val;
+// node.next = node.next.next;
+// }
+// }
+// }
