9603678e12632bcd11594b6b2b107a20c3de4f6d
Ahmed <ahmedshaikvk18@gmail.com>
2025-03-29 14:18:27 +0530

    Completed Odd Even Linked List from leetcode

---
+++ Linkedlist/medium/OddEvenLinkedList.java
@@ -0,0 +1,26 @@
+package Linkedlist.medium;
+
+public class OddEvenLinkedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
