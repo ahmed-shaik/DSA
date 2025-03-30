dc1f8222e34ea077bba17e09012b7a05397884d5
Ahmed <ahmedshaikvk18@gmail.com>
2025-03-30 10:29:41 +0530

    completed Sort List from leetcode

---
+++ Linkedlist/medium/SortList.java
@@ -0,0 +1,55 @@
+package Linkedlist.medium;
+
+public class SortList {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+
+        return merge(left, right);
+    }
+
+    private ListNode getMid(ListNode head) {
+        ListNode slow = head, fast = head, prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null; // Disconnect the two halves
+
+        return slow;
+    }
+
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                tail.next = left;
+                left = left.next;
+            } else {
+                tail.next = right;
+                right = right.next;
+            }
+            tail = tail.next;
+        }
+
+        tail.next = (left != null) ? left : right;
+        return dummy.next;
+    }
+}
