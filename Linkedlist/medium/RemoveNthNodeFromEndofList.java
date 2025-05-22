af6ffa613f422ff019779edca55881410c13c23d
Ahmed <ahmedshaikvk18@gmail.com>
2025-05-22 11:44:04 +0530

    optimized remove nth node from the last from linked list

--- Linkedlist/medium/RemoveNthNodeFromEndofList.java
+++ Linkedlist/medium/RemoveNthNodeFromEndofList.java
@@ -4,14 +4,24 @@ package Linkedlist.medium;
 public class RemoveNthNodeFromEndofList {
     /**
      * Definition for singly-linked list.
-     * public class ListNode {
-     * int val;
-     * ListNode next;
-     * ListNode() {}
-     * ListNode(int val) { this.val = val; }
-     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     * }
      */
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
     class Solution {
         // public ListNode removeNthFromEnd(ListNode head, int n) {
         // ListNode temp = head;
@@ -31,4 +41,23 @@ public class RemoveNthNodeFromEndofList {
         // temp.next = temp.next.next;
         // return head;
     }
+
+    class Solution2 {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            ListNode fast = dummy;
+            ListNode slow = dummy;
+            for (int i = 0; i <= n; i++) {
+                fast = fast.next;
+            }
+            while (fast != null) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            slow.next = slow.next.next;
+            return dummy.next;
+        }
+    }
+
 }
