6114cc3c26a4c1172491096c55e2b830c10d6b86
Ahmed <ahmedshaikvk18@gmail.com>
2025-05-23 11:10:50 +0530

    completed Merge k Sorted Lists from leetcode

---
+++ Linkedlist/hard/MergekSortedLists.java
@@ -0,0 +1,61 @@
+package Linkedlist.hard;
+
+public class MergekSortedLists {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        public ListNode mergeKLists(ListNode[] lists) {
+            if (lists == null || lists.length == 0) {
+                return null;
+            }
+
+            ListNode merged = lists[0];
+            for (int i = 1; i < lists.length; i++) {
+                merged = merge(merged, lists[i]);
+            }
+
+            return merged;
+        }
+
+        private ListNode merge(ListNode l1, ListNode l2) {
+            ListNode dummy = new ListNode(0);
+            ListNode tail = dummy;
+
+            while (l1 != null && l2 != null) {
+                if (l1.val < l2.val) {
+                    tail.next = l1;
+                    l1 = l1.next;
+                } else {
+                    tail.next = l2;
+                    l2 = l2.next;
+                }
+                tail = tail.next;
+            }
+            if (l1 != null) {
+                tail.next = l1;
+            } else {
+                tail.next = l2;
+            }
+
+            return dummy.next;
+        }
+    }
+
+}
