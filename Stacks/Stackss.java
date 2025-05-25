89baed9221736771b1c7e38b2bd84f61e8125fa3
Ahmed <ahmedshaikvk18@gmail.com>
2025-05-25 05:57:21 +0530

    completed stacks chapter

--- Stacks/Stackss.java
+++ Stacks/Stackss.java
@@ -184,9 +184,56 @@ public class Stackss {
         return false;
     }
 
+    public static int[] nextSmallerLeft(int[] heights) {
+        Stack<Integer> s = new Stack<>();
+        int[] nextSmallerLeft = new int[heights.length];
+        for (int i = 0; i < heights.length; i++) {
+            while (!s.isEmpty() && heights[s.peek()] >= heights[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nextSmallerLeft[i] = -1;
+            } else {
+                nextSmallerLeft[i] = s.peek();
+            }
+            s.push(i);
+        }
+        return nextSmallerLeft;
+    }
+
+    public static int[] nextSmallerRight(int[] heights) {
+        Stack<Integer> s = new Stack<>();
+        int[] nextSmallerRight = new int[heights.length];
+        for (int i = heights.length - 1; i >= 0; i--) {
+            while (!s.isEmpty() && heights[i] < heights[s.peek()]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nextSmallerRight[i] = heights.length;
+            } else {
+                nextSmallerRight[i] = s.peek();
+            }
+            s.push(i);
+        }
+        return nextSmallerRight;
+    }
+
+    public static int largestRectangleArea(int[] heights) {
+        int nextSmallerLeft[] = new int[heights.length];
+        int nextSmallerRight[] = new int[heights.length];
+        nextSmallerLeft = nextSmallerLeft(heights);
+        nextSmallerRight = nextSmallerRight(heights);
+        int max = 0;
+        for (int i = 0; i < heights.length; i++) {
+            int area = (nextSmallerRight[i] - nextSmallerLeft[i] - 1) * heights[i];
+            max = Math.max(max, area);
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         // stack - LIFO - Last In First Out
-        // stack - push  - pop - peek - isEmpty - size
+        // stack - push - pop - peek - isEmpty - size
         // Stack s = new Stack();
         Stack<Integer> s = new Stack<>();
         s.push(3);
@@ -228,10 +275,12 @@ public class Stackss {
         // System.out.println(validParentheses("({[()]})"));
         // System.out.println(validParentheses("((((()"));
 
-        System.out. println(duplicateParentheses("((a+b))"));
+        // System.out. println(duplicateParentheses("((a+b))"));
 
-        System.out.println(duplicateParentheses("(a-b)"));
+        // System.out.println(duplicateParentheses("(a-b)"));
 
+        int heights[] = { 2, 1, 5, 6, 2, 3 };
+        System.out.println(largestRectangleArea(heights));
     }
 
 }
