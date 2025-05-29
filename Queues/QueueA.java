cd84564f806368f4b251e8620882c4da55841f47
Ahmed <ahmedshaikvk18@gmail.com>
2025-05-29 11:08:10 +0530

    completed queue chapter

--- Queues/QueueA.java
+++ Queues/QueueA.java
@@ -200,6 +200,125 @@ public class QueueA {
         }
     }
 
+    static class StackUsingQueue {
+        static Queue<Integer> q1 = new LinkedList<>();
+        static Queue<Integer> q2 = new LinkedList<>();
+
+        public static boolean isEmpty() {
+            return q1.isEmpty() && q2.isEmpty();
+        }
+
+        public static void push(int data) {
+            if (!q1.isEmpty()) {
+                q1.add(data);
+            } else {
+                q2.add(data);
+            }
+        }
+
+        public static int pop() {
+            if (isEmpty()) {
+                System.out.println("Stack is empty");
+                return -1;
+            }
+            int top = -1;
+            if (!q1.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    if (q1.isEmpty()) {
+                        break;
+                    }
+                    q2.add(top);
+                }
+            } else {
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    if (q2.isEmpty()) {
+                        break;
+                    }
+                    q1.add(top);
+                }
+            }
+            return top;
+        }
+
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("Stack is empty");
+                return -1;
+            }
+            int top = -1;
+            if (!q1.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    q2.add(top);
+                }
+            } else {
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    q1.add(top);
+                }
+            }
+            return top;
+        }
+    }
+
+    public static void printNonRepeating(String s) {
+        int freq[] = new int[26];
+        Queue<Character> q = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            q.add(ch);
+            freq[ch - 'a']++;
+            while (!q.isEmpty() && freq[q.peek() - 'a'] > 1) {
+                q.remove();
+            }
+            if (q.isEmpty()) {
+                System.out.print(-1 + " ");
+            } else {
+                System.out.print(q.peek() + " ");
+            }
+        }
+    }
+
+    public static void interleavingQueue(Queue<Integer> q) {
+        Queue<Integer> first = new LinkedList<>();
+        int size = q.size();
+        for (int i = 0; i < size / 2; i++) {
+            first.add(q.remove());
+        }
+        while (!first.isEmpty()) {
+            q.add(first.remove());
+            q.add(q.remove());
+        }
+    }
+
+    public static void printQueue(Queue<Integer> q) {
+        while (!q.isEmpty()) {
+            System.out.print(q.peek() + " ");
+            q.remove();
+        }
+    }
+
+    public static void reverseUsingStack(Queue<Integer> q) {
+        Stack<Integer> s = new Stack<>();
+        while (!q.isEmpty()) {
+            s.push(q.remove());
+        }
+        while (!s.isEmpty()) {
+            q.add(s.pop());
+        }
+    }
+
+    public static void reverseUsingRecursion(Queue<Integer> q) {
+        if (q.isEmpty()) {
+            return;
+        }
+        int top = q.remove();
+        reverseUsingRecursion(q);
+        q.add(top);
+    }
+
     public static void main(String[] args) {
         // LinkedQueue q = new LinkedQueue();
         // q.add(1);
@@ -221,14 +340,55 @@ public class QueueA {
         // System.out.println(q.remove());
         // }
 
-        QueueUsingStacks q = new QueueUsingStacks();
-        q.add(1);
-        q.add(2);
-        q.add(3);
+        // QueueUsingStacks q = new QueueUsingStacks();
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
 
-        while (!q.isEmpty()) {
-            System.out.println(q.peek());
-            q.remove();
-        }
+        // while (!q.isEmpty()) {
+        // System.out.println(q.peek());
+        // q.remove();
+        // }
+
+        // StackUsingQueue s = new StackUsingQueue();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
+
+        // while (!s.isEmpty()) {
+        // System.out.println(s.peek());
+        // s.pop();
+        // }
+
+        // String str = "aabccxb";
+        // printNonRepeating(str);
+
+        // Queue<Integer> q = new LinkedList<>();
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
+        // q.add(4);
+        // q.add(5);
+        // q.add(6);
+        // q.add(7);
+        // q.add(8);
+        // q.add(9);
+        // q.add(10);
+        // // interleavingQueue(q);
+        // // reverseUsingStack(q);
+        // reverseUsingRecursion(q);
+        // printQueue(q);
+
+        // Deque - double ended queue operations👇
+
+        // addFirst( )
+        // addLast( )
+        // removeFirst( )
+        // removeLast( )
+        // getLast( )
+        Deque<Integer> d = new LinkedList<>();
+        d.addFirst(1);
+        d.addFirst(2);
+        System.out.println(d);
     }
 }
