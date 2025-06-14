230c0369b38178ad20560fcf1e125b96eafee15c
Ahmed <ahmedshaikvk18@gmail.com>
2025-06-14 19:07:56 +0530

    completed Binary Tree pre Order Traversal by iterationfrom leetcode

--- BinaryTree/Easy/BinaryTreePostorderTraversal.java
+++ BinaryTree/Easy/BinaryTreePostorderTraversal.java
@@ -2,6 +2,7 @@ package BinaryTree.Easy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import BinaryTree.Easy.BinaryTreePreOrderTraversal.TreeNode;
 
@@ -36,5 +37,25 @@ public class BinaryTreePostorderTraversal {
             helper(list, root.right);
             list.add(root.val);
         }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Stack<TreeNode> s = new Stack<>();
+        s.push(root);
+        while (!s.isEmpty()) {
+            TreeNode curr = s.pop();
+            list.add(curr.val);
+            if (curr.right != null) {
+                s.push(curr.right);
+            }
+            if (curr.left != null) {
+                s.push(curr.left);
+            }
+        }
+        return list;
     }
 }
--- BinaryTree/Easy/BinaryTreePreOrderTraversal.java
+++ BinaryTree/Easy/BinaryTreePreOrderTraversal.java
@@ -2,6 +2,7 @@ package BinaryTree.Easy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreePreOrderTraversal {
     /**
@@ -46,5 +47,42 @@ public class BinaryTreePreOrderTraversal {
             preOrder(node.right, li);
         }
 
+    }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution2 {
+        public List<Integer> preorderTraversal(TreeNode root) {
+            List<Integer> list = new ArrayList<>();
+            if (root == null) {
+                return list;
+            }
+            Stack<TreeNode> s = new Stack<>();
+            s.push(root);
+            while (!s.isEmpty()) {
+                TreeNode curr = s.pop();
+                list.add(curr.val);
+                if (curr.right != null) {
+                    s.push(curr.right);
+                }
+                if (curr.left != null) {
+                    s.push(curr.left);
+                }
+            }
+            return list;
+        }
     }
 }
