c173ad34ee0ebdbcf5ea88252576655ccaef979c
Ahmed <ahmedshaikvk18@gmail.com>
2025-06-14 19:50:30 +0530

    completed Binary Tree post Order Traversal by iteration using 2 stacksfrom leetcode

--- BinaryTree/Easy/BinaryTreePostorderTraversal.java
+++ BinaryTree/Easy/BinaryTreePostorderTraversal.java
@@ -63,4 +63,45 @@ public class BinaryTreePostorderTraversal {
             return list;
         }
     }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution2 {
+        public List<Integer> postorderTraversal(TreeNode root) {
+            List<Integer> list = new ArrayList<>();
+            if (root == null) {
+                return list;
+            }
+            Stack<TreeNode> s1 = new Stack<>();
+            Stack<TreeNode> s2 = new Stack<>();
+            s1.push(root);
+            while (!s1.isEmpty()) {
+                root = s1.pop();
+                s2.push(root);
+                if (root.left != null) {
+                    s1.push(root.left);
+                }
+                if (root.right != null) {
+                    s1.push(root.right);
+                }
+            }
+            while (!s2.isEmpty()) {
+                list.add(s2.pop().val);
+            }
+            return list;
+        }
+    }
 }
