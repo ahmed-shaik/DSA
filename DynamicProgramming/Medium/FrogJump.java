b4263b7600878631678965f8f2a05db6123a36a6
Ahmed <ahmedshaikvk18@gmail.com>
2025-06-30 22:30:35 +0530

    completed frog jump from gfg

---
+++ DynamicProgramming/Medium/FrogJump.java
@@ -0,0 +1,24 @@
+package DynamicProgramming.Medium;
+
+public class FrogJump {
+
+    // User function Template for Java
+    class Solution {
+        int minCost(int[] h) {
+            // code here
+            return helper(h, h.length - 1);
+        }
+
+        public static int helper(int[] h, int i) {
+            if (i == 0) {
+                return 0;
+            }
+            int o = helper(h, i - 1) + Math.abs(h[i] - h[i - 1]);
+            int t = Integer.MAX_VALUE;
+            if (i > 1)
+                t = helper(h, i - 2) + Math.abs(h[i] - h[i - 2]);
+
+            return Math.min(o, t);
+        }
+    }
+}
