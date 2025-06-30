2dac7a4a7399c5ae5b5c236b0249d85713730500
Ahmed <ahmedshaikvk18@gmail.com>
2025-07-01 04:43:40 +0530

    completed fog jump from gfg

--- DynamicProgramming/Medium/FrogJump.java
+++ DynamicProgramming/Medium/FrogJump.java
@@ -21,4 +21,24 @@ public class FrogJump {
             return Math.min(o, t);
         }
     }
+
+    // optimization
+    // User function Template for Java
+    class Solution1 {
+        int minCost(int[] h) {
+            // code here
+            int prev1 = 0;
+            int prev2 = 0;
+            for (int i = 1; i < h.length; i++) {
+                int o = prev1 + Math.abs(h[i] - h[i - 1]);
+                int t = Integer.MAX_VALUE;
+                if (i > 1)
+                    t = prev2 + Math.abs(h[i] - h[i - 2]);
+                int curr = Math.min(o, t);
+                prev2 = prev1;
+                prev1 = curr;
+            }
+            return prev1;
+        }
+    }
 }
